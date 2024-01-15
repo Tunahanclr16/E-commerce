1dd8b6bacfe2c92d826f3b8532174dc84da99700
Tunahanclrr <tunaceler@gmail.com>
2024-01-15 12:36:25 +0300

    newsletter responsive

--- src/Components/Home/Newsletter.jsx
+++ src/Components/Home/Newsletter.jsx
@@ -11,9 +11,9 @@ export default function Newsletter() {
         <div className='flex justify-center h-full items-center flex-col'>
             <div>
                 <h1 className='text-black md:text-4xl text-center sm:text-2xl text-xl '>NewsLetter</h1>
-                <p className='text-black/60 mt-1 text-[13px] font-normal leading-6'>Subscribe to our newsletters and don’t miss new arrivals, the latest fashion updates and our promotions.</p>
+                <p className='text-black/60 mt-1 text-[13px] font-normal text-center max-w-[350px] sm:max-w-full leading-6'>Subscribe to our newsletters and don’t miss new arrivals, the latest fashion updates and our promotions.</p>
                 <div className="flex justify-center items-center  mt-2 ">
-                <input onChange={emailChange} value={email}  type="email" placeholder='enter your email adresss' className='leading-10 bg-transparent  w-[346px] rounded  text-[#777] h-10 border-2 outline-none border-[#8a8a8a] '/>
+                <input onChange={emailChange} value={email}  type="email" placeholder='enter your email adresss' className='leading-10 bg-transparent p-2 w-[250px]  sm:w-[346px] rounded  text-[#777] h-10 border-2 outline-none border-[#8a8a8a] '/>
                 <div className='flex items-center text-gray-400 bg-transparent hover:bg-black hover:text-white transition-all border-2 border-[#8a8a8a] cursor-pointer h-10 w-11 justify-center'>
                     <button type='submit'>
                     <MdNavigateNext className='' size={32}/>
